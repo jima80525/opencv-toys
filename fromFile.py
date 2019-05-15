1fb95da8e65fcad01487a94aeaed2fb38ce4e63b
Jim Anderson <jim.anderson@se.com>
2019-05-15 15:43:08 -0600

    Major rewrite of motion script.  Split functionality for actual detection into separate function.  Added reading yuv files

--- fromFile.py
+++ fromFile.py
@@ -7,12 +7,32 @@ import cv2
 import numpy as np
 
 
-def readFrame(fp, frame_len, shape):
-    while fp.read(frame_len):
-        raw = fp.read(frame_len)
-        yuv = np.frombuffer(raw, dtype=np.uint8)
-        yuv = yuv.reshape(shape)
-        yield cv2.cvtColor(yuv, cv2.COLOR_YUV2BGR_NV21)
+def readFrame(filename, width, height):
+    with open(filename, 'rb') as fp:
+        frame_len = int(width * height * 3 / 2)
+        shape = (int(height*1.5), width)
+        while True:
+            raw = fp.read(frame_len)
+            if not raw:
+                break
+            yuv = np.frombuffer(raw, dtype=np.uint8)
+            yuv = yuv.reshape(shape)
+            yield cv2.cvtColor(yuv, cv2.COLOR_YUV2BGR_NV21)
+
+
+def readYFrame(filename, width, height):
+    with open(filename, 'rb') as fp:
+        frame_len = width * height
+        extra_len = int(width * (height / 2))
+        shape = (height, width)
+        while True:
+            raw = fp.read(frame_len)
+            if not raw:
+                break
+            extra = fp.read(extra_len)
+            yuv = np.frombuffer(raw, dtype=np.uint8)
+            yuv = yuv.reshape(shape)
+            yield yuv
 
 
 def get_args():
@@ -25,10 +45,6 @@ if __name__ == "__main__":
     args = get_args()
     width = 1024
     height = 576
-    frame_len = int(width * height * 3 / 2)
-    shape = (int(height*1.5), width)
-
-    with open(args.filename, 'rb') as fp:
-        for frame in readFrame(fp, frame_len, shape):
-            cv2.imshow("frame", frame)
-            cv2.waitKey(30)
+    for frame in readYFrame(args.filename, width, height):
+        cv2.imshow("frame", frame)
+        cv2.waitKey(30)
--- motion.py
+++ motion.py
@@ -19,7 +19,6 @@ class Window():
         self.title = title
         if self.visible:
             print(title, self.width, self.next_x_pos, visible)
-            # cv2.namedWindow(self.title, cv2.WINDOW_NORMAL)
             cv2.namedWindow(self.title, cv2.WINDOW_AUTOSIZE)
             cv2.moveWindow(self.title, self.next_x_pos, self.next_y_pos)
             # print(f"Moving to {
@@ -35,148 +34,200 @@ def get_args():
     parser.add_argument('-f', '--filename', help='video file to process')
     parser.add_argument('-i', '--ip_addr',
                         help='ip address from which to stream')
+    parser.add_argument('-y', '--yuvfile', help='file of yuv stream frames')
     parser.add_argument('-d', '--show-dilation', action='store_true',
                         help='display mask after dilation')
-    parser.add_argument('-o', '--show-original', action='store_true',
-                        help='display original image')
     parser.add_argument('-e', '--show-erosion', action='store_true',
                         help='display mask after erosion')
     parser.add_argument('-m', '--show-mask', action='store_true',
                         help='display initial mask')
-    parser.add_argument('-r', '--rotate-rect', action='store_true',
-                        help='allow bounding rectable to be angled')
+    parser.add_argument('-o', '--show-original', action='store_true',
+                        help='display original image')
     parser.add_argument('--min-area', type=int, default=1000,
                         help='smallest object size')
+    parser.add_argument('-r', '--show-rects', action='store_true',
+                        help='show rects on output instead of cells')
     args = parser.parse_args()
     return args
 
 
-def get_background_subtractor(args):
-    # JHA TODO add options for picking algorithm
-    fgbg = cv2.createBackgroundSubtractorMOG2()
-    # fgbg = cv2.bgsegm.createBackgroundSubtractorMOG()
-    # fgbg = cv2.bgsegm.createBackgroundSubtractorGSOC()
-    # fgbg = cv2.bgsegm.createBackgroundSubtractorGMG()
-    # fgbg = cv2.createBackgroundSubtractorCNT()
-    # fgbg = cv2.bgsegm.createBackgroundSubtractorLSBP()
-    return fgbg
+class FileReader():
+    def __init__(self, filename):
+        self.cap = cv2.VideoCapture(filename)
+        self.width = int(self.cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+        self.height = int(self.cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+
+    def __del__(self):
+        self.cap.release()
+
+    def get_frame(self):
+        while True:
+            ret, frame = self.cap.read()
+            if not ret:
+                break
+            yield frame
 
 
-def get_frame(cap, frames_to_skip):
-    while frames_to_skip:
-        ret, frame = cap.read()
-        if not ret:
-            return None, None
-        frames_to_skip -= 1
+class YuvReader():
+    def __init__(self, filename):
+        self.fp = open(filename, 'rb')
+        # JHA TODO figure out how to deal with this
+        self.width = 1024
+        self.height = 576
+        self.frame_len = self.width * self.height
+        self.extra_len = int(self.width * (self.height / 2))
+        self.shape = (self.height, self.width)
 
-    return cap.read()
+    def __del__(self):
+        self.fp.close()
 
+    def get_frame(self):
+        while True:
+            raw = self.fp.read(self.frame_len)
+            if not raw:
+                break
+            extra = self.fp.read(self.extra_len)
+            yuv = numpy.frombuffer(raw, dtype=numpy.uint8)
+            yuv = yuv.reshape(self.shape)
+            yield yuv
+
+
+class CameraReader():
+    def __init__(self, ip_addr):
+        filename = 'rtsp://{}/stream1'.format(ip_addr)
+        self.cap = cv2.VideoCapture(filename)
+        self.width = int(self.cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+        self.height = int(self.cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+
+    def __del__(self):
+        self.cap.release()
+
+    def get_frame(self):
+        while True:
+            frames_to_skip = 10
+            while frames_to_skip:
+                ret, frame = self.cap.read()
+                if not ret:
+                    break
+                frames_to_skip -= 1
+
+            ret, frame = self.cap.read()
+            if not ret:
+                break
+            yield frame
+
+
+class SAEngine:
+    def __init__(self, width, height, args):
+        self.fgbg = cv2.createBackgroundSubtractorMOG2()
+        self.kernel = numpy.ones((5, 5), numpy.uint8)
+        self.mask = Window('mask', args.show_mask)
+        self.dilation = Window('dilation', args.show_dilation)
+        self.erosion = Window('erosion', args.show_erosion)
+        self.mask = Window('mask', args.show_mask)
+        self.dilation = Window('dilation', args.show_dilation)
+        self.erosion = Window('erosion', args.show_erosion)
+        self.min_area = args.min_area
+
+    def get_motion_rects(self, frame):
+        fgmask = self.fgbg.apply(frame)
+
+        self.mask.show(fgmask)
+
+        # JHA TODO set flag for iterations?
+        erosion_frame = cv2.erode(fgmask, self.kernel, iterations=2)
+        dilation_frame = cv2.dilate(erosion_frame, self.kernel, iterations=2)
+        self.dilation.show(dilation_frame)
+        self.erosion.show(erosion_frame)
+
+        (contours, hierarchy) = cv2.findContours(dilation_frame,
+                                                 cv2.RETR_EXTERNAL,
+                                                 cv2.CHAIN_APPROX_NONE)
+
+        rects = [cv2.boundingRect(contour) for contour in contours if
+                 cv2.contourArea(contour) > self.min_area]
+        return rects
+
+
+def drawRects(frame, rects):
+    """ Draws bounding boxes from motion detection. """
+    for x, y, w, h in rects:
+        cv2.rectangle(frame, (x, y), (x+w, y+h), (255, 0, 0), 1)
+
+
+def drawCells(frame, rects, width, height):
+    """ Highlights the 16x16 pixel cells which contain motion. """
+    def roundDown(val):
+        return math.floor(val/16) * 16
+    def roundUp(val):
+        return math.ceil(val/16) * 16
+
+    # draw grid
+    for x in range(0, width, 16):
+        cv2.line(frame, (x, 0), (x, height), (255, 0, 0))
+    for y in range(0, height, 16):
+        cv2.line(frame, (0, y), (width, y), (255, 0, 0))
+
+
+    # create an overlay frame
+    img = numpy.zeros((height, width), numpy.uint8)
+
+    for x, y, w, h in rects:
+        # now draw the cells on the overlay frame
+        x0 = roundDown(x)
+        y0 = roundDown(y)
+        x1 = roundUp(x + w)
+        y1 = roundUp(y + h)
+        cv2.rectangle(img, (x0, y0), (x1, y1), (255, 0, 0), -3)
+
+    #####
+    # from:
+    # https://stackoverflow.com/questions/46103731/is-there-a-simple-method-to-highlight-the-mask/46105196#46105196
+    b,g,r = cv2.split(frame)
+    g = cv2.add(g, 90, dst = g, mask = img, dtype = cv2.CV_8U)
+    cv2.merge((b,g,r), frame)
+    #####
 
 def main():
     args = get_args()
 
-    fgbg = get_background_subtractor(args)
     if args.filename:
-        filename = args.filename
-        skip_frames = 0
+        cap = FileReader(args.filename)
+    elif args.yuvfile:
+        cap = YuvReader(args.yuvfile)
+        # JHA todo - cell drawing is based on 3 color frames.  For YUV we're
+        # only doing Y field
+        args.show_rects = True
     else:
-        filename = 'rtsp://{}/stream1'.format(args.ip_addr)
-        skip_frames = 10
+        cap = CameraReader(args.ip_addr)
 
-    cap = cv2.VideoCapture(filename)
-
-    Window.width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
-    Window.height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+    Window.width = cap.width
+    Window.height = cap.height
 
     original = Window('original frame', args.show_original)
     output = Window(f"Cell-based Motion ({Window.width}x{Window.height})", True)
-    mask = Window('mask', args.show_mask)
-    dilation = Window('dilation', args.show_dilation)
-    erosion = Window('erosion', args.show_erosion)
 
-    kernel = numpy.ones((5, 5), numpy.uint8)
+    engine = SAEngine(cap.width, cap.height, args)
 
-    for loop_count in itertools.count():
-        ret, frame = get_frame(cap, skip_frames)
-        if not ret:
-            break
-
-        # frame = cv2.resize(frame, (1024, 576), interpolation=cv2.INTER_AREA)
-        # frame = cv2.resize(frame, (426, 240), interpolation=cv2.INTER_AREA)
-        # frame = cv2.resize(frame, (480, 640), interpolation=cv2.INTER_AREA)
-        # frame = cv2.resize(frame, (640, 480), interpolation=cv2.INTER_AREA)
-        # frame = cv2.resize(frame, (320, 240), interpolation=cv2.INTER_AREA)
+    for frame in cap.get_frame():
 
         original.show(frame)
-        fgmask = fgbg.apply(frame)
-        mask.show(fgmask)
 
-        if False:
-            dilation_frame = cv2.dilate(fgmask, kernel, iterations=2)
-            erosion_frame = cv2.erode(dilation_frame, kernel, iterations=2)
+        rects = engine.get_motion_rects(frame)
+
+        if args.show_rects:
+            drawRects(frame, rects)
         else:
-            erosion_frame = cv2.erode(fgmask, kernel, iterations=2)
-            dilation_frame = cv2.dilate(erosion_frame, kernel, iterations=2)
-
-        (contours, hierarchy) = cv2.findContours(erosion_frame,
-                                                 cv2.RETR_EXTERNAL,
-                                                 cv2.CHAIN_APPROX_NONE)
-
-        img = numpy.zeros(erosion_frame.shape, numpy.uint8)
-        def roundDown(val):
-            return math.floor(val/16) * 16
-        def roundUp(val):
-            return math.ceil(val/16) * 16
-
-        # draw grid
-        h, w = erosion_frame.shape
-        for x in range(0, w, 16):
-            cv2.line(frame, (x, 0), (x, h), (255, 0, 0))
-        for y in range(0, h, 16):
-            cv2.line(frame, (0, y), (w, y), (255, 0, 0))
-
-
-        for contour in contours:
-            area = cv2.contourArea(contour)
-            if area > args.min_area:
-                if args.rotate_rect:
-                    rect = cv2.minAreaRect(contour)
-                    box = cv2.boxPoints(rect)
-                    box = numpy.int0(box)
-                    cv2.drawContours(frame, [box], 0, (255, 0, 0), 2)
-                else:
-                    x, y, w, h = cv2.boundingRect(contour)
-                    # draw the orig rect on the frame
-                    # cv2.rectangle(frame, (x, y), (x+w, y+h), (255, 0, 0), 1)
-
-                    # now draw the cells on the overlay frame
-                    x0 = roundDown(x)
-                    y0 = roundDown(y)
-                    x1 = roundUp(x + w)
-                    y1 = roundUp(y + h)
-
-                    cv2.rectangle(img, (x0, y0), (x1, y1), (255, 0, 0), -3)
-
-        #####
-        # from:
-        # https://stackoverflow.com/questions/46103731/is-there-a-simple-method-to-highlight-the-mask/46105196#46105196
-        b,g,r = cv2.split(frame)
-        g = cv2.add(g, 90, dst = g, mask = img, dtype = cv2.CV_8U)
-        cv2.merge((b,g,r), frame)
-        #####
+            drawCells(frame, rects, cap.width, cap.height)
 
         output.show(frame)
-        dilation.show(dilation_frame)
-        erosion.show(erosion_frame)
-        # input()
-        # print("new frame")
 
         k = cv2.waitKey(30) & 0xff
         if k == 27:
             break
-    cap.release()
     cv2.destroyAllWindows()
 
 
 if __name__ == '__main__':
     main()
+# frame = cv2.resize(frame, (1024, 576), interpolation=cv2.INTER_AREA)
